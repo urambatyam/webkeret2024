c6796e915996bac919efc416881d37175e41878c
h270519 <h270519@stud.u-szeged.hu>
2024-04-13 20:17:38 +0200

    alam

--- Weboldal/src/environments/environment.prod.ts
+++ Weboldal/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: { 
+    projectId:"webfejlesztes-2024",
+    appId:"1:401086229314:web:ea29334972e013b755f6db",
+    storageBucket:"webfejlesztes-2024.appspot.com",
+    apiKey:"AIzaSyDpKBSKb1zR6APYbjKI--7ncfDbThSQ1Sk",
+    authDomain:"webfejlesztes-2024.firebaseapp.com",
+    messagingSenderId:"401086229314",
+    measurementId:"G-CCYKPJ6P8H"
+},
+
 };
--- Weboldal/src/environments/environment.ts
+++ Weboldal/src/environments/environment.ts
@@ -3,15 +3,20 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  firebase: { projectId:"webfejlesztes-2024",
-  appId:"1:401086229314:web:ea29334972e013b755f6db",
-  storageBucket:"webfejlesztes-2024.appspot.com",
-  apiKey:"AIzaSyDpKBSKb1zR6APYbjKI--7ncfDbThSQ1Sk",
-  authDomain:"webfejlesztes-2024.firebaseapp.com",
-  messagingSenderId:"401086229314",
-  measurementId:"G-CCYKPJ6P8H"},
-  production: false
+  production: false,
+  firebase: { 
+    projectId:"webfejlesztes-2024",
+    appId:"1:401086229314:web:ea29334972e013b755f6db",
+    storageBucket:"webfejlesztes-2024.appspot.com",
+    apiKey:"AIzaSyDpKBSKb1zR6APYbjKI--7ncfDbThSQ1Sk",
+    authDomain:"webfejlesztes-2024.firebaseapp.com",
+    messagingSenderId:"401086229314",
+    measurementId:"G-CCYKPJ6P8H"
+},
+
 };
+    //provideFirebaseApp(() => initializeApp({"projectId":"webfejlesztes-2024","appId":"1:401086229314:web:ea29334972e013b755f6db","storageBucket":"webfejlesztes-2024.appspot.com","apiKey":"AIzaSyDpKBSKb1zR6APYbjKI--7ncfDbThSQ1Sk","authDomain":"webfejlesztes-2024.firebaseapp.com","messagingSenderId":"401086229314","measurementId":"G-CCYKPJ6P8H"})),
+
 
 /*
  * For easier debugging in development mode, you can import the following file
